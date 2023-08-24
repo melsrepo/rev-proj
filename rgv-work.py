10ed68da30f52e8350fb673c255e9cda5711554f
Carmel <heydarianc@gmail.com>
2023-08-24 17:58:30 -0600

    trying to combine the two list as one dictionary

--- rgv-work.py
+++ rgv-work.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
@@ -6,20 +7,31 @@ def getData(url):
     response = requests.get(url)
     respTxt = BeautifulSoup(response.text,
                             "lxml") #converting the html to text
+    #gets all the organizarion names
     orgs = respTxt.find_all("h3", attrs={"aria-expanded": "true"})
+    #gets the org info
+    info = respTxt.find_all("p")
+    #initialize keys
+    res = dict(zip(orgs, info))
+
     arr = []
     # for elements in orgs:
     #     arr.append(elements.text)
     # for i in arr:
     #     print(i)
-    info = respTxt.find_all("p")
-    arr2 = []
-    for elements in info:
-        arr2.append(elements.text)
-    for i in arr2:
-        print(i)
 
+    # arr2 = []
+    # for elements in info:
+    #     arr2.append(elements.text)
+    # for i in arr2:
+    #     print(i)
+    # return arr2
+    for keys, value in res.items():
+        print(keys)
+
+def exportDate(data):
+    df = pd.DataFrame(data)
+    df.to_excel("rgv-sheet.xlsx")
 getData("https://rgvpartnership.com/rgv-non-profit-organizations/?fbclid=IwAR1pt2PXiA7R94dHuUgvsWGURo7eLTKDySVz6t5dS_6xb1fFn1MH2jAnDkQ")
-
-
+#exportDate(d)
 
