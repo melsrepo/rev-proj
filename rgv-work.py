8e46b5df34bbcffb8c2d1a89b98c1e508447d401
Carmel <heydarianc@gmail.com>
2023-08-25 16:15:15 -0600

    cleaned up the file. Still two rows

--- rgv-work.py
+++ rgv-work.py
@@ -13,7 +13,6 @@ def getData(url):
     #gets the org info
     info = respTxt.find_all("p")
 
-
     #all the orgs are now in one list
     orgList = []
     for elements in orgs:
@@ -24,27 +23,18 @@ def getData(url):
     #does not have a new line character
     for elements in info:
         infoList.append(elements.text)
-    # return arr2
-    # for keys, value in res.items():
-    #     print(keys)
     #combine the lists into a dictionary
-    #orgInfoList = dict(zip(orgList, infoList))
+    orgInfoList = dict(zip(orgList, infoList))
 
-    # data = []
-    # for elem in orgINfoList:
-    #     data.append(elem)
-    # return data
-    #orgInfoList = {"Organization" :"", "Information": ""}
-    # orgInfoList["Organization"] = orgList
-    # orgInfoList["Information"] = infoList
-    #return orgInfoList
+    return orgInfoList
 
     #they are not the same length
-    print(len(orgList))
-    print(len(infoList))
+    # print(len(orgList))
+    # print(len(infoList))
 def exportDate(data):
     df = pd.DataFrame(data, index = [0])
     df.to_excel("rgv-sheet.xlsx")
 d = getData("https://rgvpartnership.com/rgv-non-profit-organizations/?fbclid=IwAR1pt2PXiA7R94dHuUgvsWGURo7eLTKDySVz6t5dS_6xb1fFn1MH2jAnDkQ")
 exportDate(d)
+
 
