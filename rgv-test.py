4ba3355fddff918453447817212f2c9ddb48dcf0
Carmel <heydarianc@gmail.com>
2023-08-28 13:45:17 -0600

    have a dictionary with keys: org names values: entire <p> for that element

--- rgv-test.py
+++ rgv-test.py
@@ -15,8 +15,18 @@ def getData(url):
     for elem in orgDiv:
         orgs.append(elem.find("h3").text)
         info.append(elem.find("p"))
+
+
+    #attempt at accessing the bolded into for future organizeing
+    # for i in info:
+    #     print(i.find_all("b"))
+    #combine the lists into a dictionary
+    orgInfoList = dict(zip(orgs, info))
+    print(len(orgInfoList))
     print(len(orgs))
     print(len(info))
+    for i in orgInfoList:
+        print(i, orgInfoList[i])
 
 url = "https://rgvpartnership.com/rgv-non-profit-organizations/?fbclid=IwAR05x4U45A48LqJ7XggcW5eIxy9b2DK9OkGmdBYck3sWjtQy_3U4sYMiMns"
 getData(url)
