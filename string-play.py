993a5cbc98fafa2b6cf78fa93cb3fef64bec6f14
cheljur <chelseyjurado@gmail.com>
2023-08-29 20:14:19 -0700

    Nice Dictionary Demo
    a demo for our replace, split, and dictionaries

--- string-play.py
+++ string-play.py
@@ -1,6 +1,23 @@
 store = []
 str = 'Contact: DionicioEmail: diongartist@yahoo.comPhone: 1.956.778.5854Address: 1221 W Campbell RdCity: Richardson'
-ind = str.find("Email")
+str = str.replace('Email', ',Email').replace('Phone', ',Phone').replace('Address', ',Address').replace('City', ',City')
+output = str.split(',')
+print("our first split:")
+print(output)
+print("-----------------------")
+print("our second...etc split")
+dict = {}
+for info in output:
+    info = info.split(':')
+    print(info)
+    dict[info[0].strip()] = info[1].strip()
+print("-----------------------")
+print("our nice dict")
+
+for key in dict:
+    print(key + "--" + dict[key])
+
+'''ind = str.find("Email")
 section = str[:ind]
 print(section)
 str = str.replace(section, "")
@@ -22,4 +39,4 @@ ind = str.find("City")
 section = str[:ind]
 print(section)
 str = str.replace(section, "")
-print(str)
+print(str)'''
